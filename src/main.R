8052425aa523948853c553840fc4d9cce3e64412
IraitzM <iraitzm@gmail.com>
2020-08-18 13:12:47 +0200

    First commit

---
+++ src/main.R
@@ -0,0 +1,31 @@
+# This is a simple R script to test rredis library
+library(rredis)
+
+# Running on local
+redisConnect(host = 'localhost')
+
+# Check is key exists
+redisExists('foo')
+# Assign a value
+redisSet('foo', 10)
+# Retrieve
+redisGet('foo')
+# Update value to something different
+redisSet('foo', runif(10))
+# Delete value
+redisDelete('foo')
+
+# Assign using a value map or list
+redisMSet(list(x=pi,y='Cazart',z=runif(2)))
+# Retrieve a set of keys
+redisMGet(list('z','y'))
+
+# Scan
+l <- redisKeys()
+redisMGet(l)
+
+# Flush all data
+redisFlushAll()
+
+# Close
+redisClose()
